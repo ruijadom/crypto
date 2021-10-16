30fec520969b05005d10130272bccfadc4aaa9c3
ruijadom <ruijadomingues@gmail.com>
2021-10-16 10:53:23 +0100

    simplified prop for cryptocurrency component

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-const Cryptocurrencies = () => {
+import { useGetCryptosQuery } from "../services/cryptoApi";
+export interface CryptocurrenciesProps {
+  simplified?: boolean;
+}
+
+const Cryptocurrencies = ({ simplified = false }: CryptocurrenciesProps) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isLoading } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log(cryptos);
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((item: any) =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <div>
-      <h1>Cryptocurrencies</h1>
-    </div>
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency: any) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency?.id}
+          >
+            <Link to={`/crypto/${currency?.id}`}>
+              <Card
+                title={`${currency?.rank}. ${currency?.name}`}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={currency?.iconUrl}
+                    alt="coin icon"
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(currency?.price)}</p>
+                <p>Market Cap: {millify(currency?.marketCap)}</p>
+                <p>Daily Change: {millify(currency?.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,27 +3,69 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Link } from "react-router-dom";
+import { Cryptocurrencies, News } from "./";
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery({});
+  const { data, isLoading } = useGetCryptosQuery(10);
   console.log(data);
+  const globalStats = data?.data?.stats;
 
   return (
     <>
-      <Title level={2} className="heading">
-        Global Crypto Stats
-      </Title>
-      <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
-          <Statistic title="Total Exchanges" value="5" />
-          <Statistic title="Total Market Cap" value="5" />
-          <Statistic title="Total 24h Volume" value="5" />
-          <Statistic title="Total Market" value="5" />
-        </Col>
-      </Row>
+      {isLoading ? (
+        <p>"Loading ..."</p>
+      ) : (
+        <>
+          <Title level={2} className="heading">
+            Global Crypto Stats
+          </Title>
+          <Row>
+            <Col span={12}>
+              <Statistic
+                title="Total Cryptocurrencies"
+                value={globalStats?.total}
+              />
+              <Statistic
+                title="Total Exchanges"
+                value={millify(globalStats?.totalExchanges)}
+              />
+              <Statistic
+                title="Total Market Cap"
+                value={millify(globalStats?.totalMarketCap)}
+              />
+              <Statistic
+                title="Total 24h Volume"
+                value={millify(globalStats?.total24hVolume)}
+              />
+              <Statistic
+                title="Total Market"
+                value={millify(globalStats?.totalMarkets)}
+              />
+            </Col>
+          </Row>
+          <div className="home-heading-container">
+            <Title level={2} className="home-title">
+              Top 10 Cryptocurrencies
+            </Title>
+            <Title level={3} className="show-more">
+              <Link to="/cryptocurrencies">Show More</Link>
+            </Title>
+          </div>
+          <Cryptocurrencies simplified={true} />
+          <div className="home-heading-container">
+            <Title level={2} className="home-title">
+              Latest Crypto News
+            </Title>
+            <Title level={3} className="show-more">
+              <Link to="/news">Show More</Link>
+            </Title>
+          </div>
+          {/* <News simplified /> */}
+        </>
+      )}
     </>
   );
 };
